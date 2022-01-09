30477846b6e760b3c000080faf13cf8b358f548f
makeit-mike <mike.isaac.jones@gmail.com>
2022-01-09 15:44:06 -0600

    adding comments

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -2,15 +2,18 @@ use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
+    // print line manual
     println!("Hello world.\r\n");
     print_line("Hello, World!".to_string());
     ferris_say("Hello there".to_string());
 }
 
+// print line by function abstaction
 fn print_line(message: String){
     println!("{}", message);
 }
 
+// Print by a dependency
 fn ferris_say(message: String){
     let stdout = stdout();
     let width = message.chars().count();
