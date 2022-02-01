913832db560fb839f93181d7e7d7c43b69c30323
makeit-mike <mike.isaac.jones@gmail.com>
2022-01-31 18:25:58 -0600

    adding in async json method

--- examples/json-parsing/src/main.rs
+++ examples/json-parsing/src/main.rs
@@ -1,6 +1,31 @@
 extern crate serde_json;
+extern crate reqwest;
+extern crate futures;
+
+#[derive(serde::Deserialize, Debug)]
+struct User {
+    login: String,
+    id: u32,
+}
+
+async fn parseJson() -> Result<(), reqwest::Error> {
+    let request_url = format!("https://api.github.com/repos/{owner}/{repo}/stargazers",
+                              owner = "makeit-mike",
+                              repo = "rust_archives");
+    println!("{}", request_url);
+    let response = reqwest::get(&request_url).await?;
+
+    let users: Vec<User> = response.json().await?;
+    println!("{:?}", users);
+    Ok(())
+}
 
 fn main() {
+    let res = parseJson();
+    futures::executor::block_on(res);
+}
+
+fn main2() {
     let json_str = r#"
         {
             "name":"michael",
@@ -8,6 +33,7 @@ fn main() {
             "is_male": true
         }
     "#;
+    let json_from_url = "https://randomuser.me/api/";
 
     let result = serde_json::from_str(json_str);
 
